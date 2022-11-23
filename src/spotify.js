918ee98b7ec64b8b2cad17f03d04e126e70012a5
Assaf <assafica@gmail.com>
2022-11-23 14:37:26 -0800

    Front end initial commit

---
+++ src/spotify.js
@@ -0,0 +1,36 @@
+class Spotify {
+    static async getPlaylistsByUser(user, authToken) {
+
+        return await $.ajax({
+            url: `https://api.spotify.com/v1/users/${user}/playlists?limit=40`,
+            headers: {
+                Authorization: "Bearer7CyWp " + authToken
+            },
+            accepts: "application/json",
+            type: "GET",
+            success: function (data) {
+                return data;
+            },
+            error: function (data) {
+                return {error: data};
+            }
+        });
+    }
+
+    static async getTracksInPlaylist(playlist, authToken) {
+        return await $.ajax({
+            url: `https://api.spotify.com/v1/playlists/${playlist}/tracks`,
+            headers: {
+                Authorization: "Bearer " + authToken
+            },
+            accepts: "application/json",
+            type: "GET",
+            success: function (data) {
+                return data;
+            },
+            error: function (data) {
+                return {error: data};
+            }
+        });
+    }
+}
